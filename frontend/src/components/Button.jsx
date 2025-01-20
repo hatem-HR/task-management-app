70728b9f5f68caf3ae8d7d5749d64daf25fca0d4
hatemhr <hatem2002@live.com>
2025-01-20 18:19:52 +0100

    first commit

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,21 @@
+
+
+function Button({
+  children,
+ 
+  bgColor = "bg-blue-600",
+  textColor,
+  className = "",
+  ...props
+}) {
+  return (
+    <button
+      className={`px-4 py-2 ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center content-center gap-4">
+      <h1>Made with ❤ by Razak |</h1>
+      <a
+        href="https://github.com/razak571"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub |
+      </a>
+      <a
+        href="https://linkedin.com/in/razakattar"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn |
+      </a>
+    
+    </div>
+  );
+};
+export default Footer;
